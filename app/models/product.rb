afb6f5f0ebeca9b9265081d59cdeabbf9e2f8995
hungnt <acv12345>
2018-01-11 16:14:21 +0700

    use hook

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,24 @@ class Product < ActiveRecord::Base
  validates :title, :description, presence: true
  validate :title_is_shorter_than_description
  validates :price, presence: true, numericality: { greater_than: 0 }
+ before_save :strip_html_from_description
+ before_save :lower_case_title_2
+ #before_validation :lower_case_title
+
+ scope :active,        -> { where(published: true) }
+ scope :priced_more_than, -> (price) { where('price > ?', price) }
+
+ def strip_html_from_description
+   self.description = ActionView::Base.full_sanitizer.sanitize(self.description)
+ end
+
+ def lower_case_title
+   self.title = title.downcase
+ end
+
+ def lower_case_title_2
+   self.title = title.downcase
+ end
 
  def title_is_shorter_than_description
  	return if title.blank? or description.blank?
@@ -9,8 +27,5 @@ class Product < ActiveRecord::Base
  		errors.add(:description, 'can not be shorter han title')
  	end
  end
-
- scope :active,        -> { where(published: true) }
- scope :priced_more_than, -> (price) { where('price > ?', price) }
 
 end

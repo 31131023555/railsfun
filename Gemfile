efb0ca72f3e19d3acf9f3206340b8c6cc862b560
hungnt <acv12345>
2018-01-12 15:57:47 +0700

    Add new product to products table and show it on browser

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+# Formtastic gem
+gem 'formtastic', '~> 3.0'
 # Bundle all countries
 gem 'countries'
 # Use enumerize to choose one of three levels
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,19 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    return redirect_to products_path if @product.save
+    render :new
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :published, :category_id)
+  end
 end

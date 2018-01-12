7b779ed7034edbd75d2fb753db163f866b2b33e8
hungnt <acv12345>
2018-01-12 14:34:02 +0700

    Show all description of selected product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,24 +4,20 @@ class Product < ActiveRecord::Base
  validates :title, :description, presence: true
  validate :title_is_shorter_than_description
  validates :price, presence: true, numericality: { greater_than: 0 }
- #before_save :strip_html_from_description
- #before_save :lower_case_title_2
- #before_validation :lower_case_title
+ before_save :strip_html_from_description
+ before_save :lower_case_title
 
-# scope :active,        -> { where(published: true) }
-# scope :priced_more_than, -> (price) { where('price > ?', price) }
+ scope :active,        -> { where(published: true) }
+ scope :priced_more_than, -> (price) { where('price > ?', price) }
 
- #def strip_html_from_description
- #  self.description = ActionView::Base.full_sanitizer.sanitize(self.description)
- #end
+ def strip_html_from_description
+   self.description = ActionView::Base.full_sanitizer.sanitize(self.description)
+ end
 
- #def lower_case_title
- #  self.title = title.downcase
- #end
+ def lower_case_title
+   self.title = title.downcase
+ end
 
- #def lower_case_title_2
- #  self.title = title.downcase
- #end
 
  enumerize :level, in: [:easy, :medium, :hard]
  enumerize :country, in: ISO3166::Country.all_translated
